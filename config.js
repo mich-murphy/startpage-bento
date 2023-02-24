6c246d523a36d0ae9a8ed91376101b4ee21f6940
Michael Murphy <github@elmurphy.com>
2023-02-24 13:25:45 +1100

    making theme default to dark

--- config.js
+++ config.js
@@ -43,8 +43,8 @@ const CONFIG = {
 
 	// Autochange by hour options (24hrs format, string must be in: hh:mm)
 	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
+	hourDarkThemeActive: '00:00',
+	hourDarkThemeInactive: '23:59',
 
 	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
 	// ├┴┐│ │ │  │ │ ││││└─┐
